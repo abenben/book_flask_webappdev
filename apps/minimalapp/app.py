167bfc765da011ed553d5b12957bd713f392fc60
abenben <abenbenben@gmail.com>
2022-02-13 14:20:35 +0900

    修正

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, current_app, g
 
 app = Flask(__name__)
 
@@ -22,3 +22,11 @@ with app.test_request_context():
     print(url_for('index'))
     print((url_for('hello-endpoint', name='world')))
     print(url_for('show_name', name='ichiro', page=1))
+
+ctx = app.app_context()
+ctx.push()
+
+print(current_app.name)
+
+g.connections = "connection"
+print(g.connections)
