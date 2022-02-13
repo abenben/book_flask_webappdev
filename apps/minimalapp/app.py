faee8789f7024fc87540839131adc32511f2c91a
abenben <abenbenben@gmail.com>
2022-02-13 13:53:48 +0900

    修正

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello Flaskbook!"
+
+
+@app.route('/hello/<name>', methods=['GET', 'POST'], endpoint='hello-endpoint')
+def hello(name):
+    return f"Hello, World!,{name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
