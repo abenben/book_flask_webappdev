5b817f659179d35abc93743b60d1e95465b38b5b
abenben <abenbenben@gmail.com>
2022-02-13 14:10:25 +0900

    修正

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -16,3 +16,9 @@ def hello(name):
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print((url_for('hello-endpoint', name='world')))
+    print(url_for('show_name', name='ichiro', page=1))
