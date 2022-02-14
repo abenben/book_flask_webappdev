b322dc63f6247161fb2aaf60a5454deffbfe6f96
abenben <abenbenben@gmail.com>
2022-02-15 03:55:15 +0900

    アンケートフォーム作成中

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, current_app, g, request
+from flask import Flask, render_template, url_for, current_app, g, request, redirect
 
 app = Flask(__name__)
 
@@ -33,3 +33,16 @@ print(g.connections)
 
 with app.test_request_context("/users?updated=true"):
     print(request.args.get('updated'))
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=['GET', 'POST'])
+def contact_complete():
+    if request.method == 'POST':
+        return redirect(url_for('contact_complete'))
+    else:
+        return render_template("contact_complete.html")
