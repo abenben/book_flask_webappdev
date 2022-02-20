22b9124b737ca887351d498c52b3bc901c9210a4
abenben <abenbenben@gmail.com>
2022-02-20 10:26:01 +0900

    問い合わせ入力チェック

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, url_for, current_app, g, request, redirect
+from email_validator import validate_email, EmailNotValidError
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    current_app,
+    g,
+    request,
+    redirect,
+    flash,
+)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Hello Flaskbook!"
 
 
-@app.route('/hello/<name>', methods=['GET', 'POST'], endpoint='hello-endpoint')
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, World!,{name}!"
 
@@ -19,9 +30,9 @@ def show_name(name):
 
 
 with app.test_request_context():
-    print(url_for('index'))
-    print((url_for('hello-endpoint', name='world')))
-    print(url_for('show_name', name='ichiro', page=1))
+    print(url_for("index"))
+    print((url_for("hello-endpoint", name="world")))
+    print(url_for("show_name", name="ichiro", page=1))
 
 ctx = app.app_context()
 ctx.push()
@@ -32,7 +43,7 @@ g.connections = "connection"
 print(g.connections)
 
 with app.test_request_context("/users?updated=true"):
-    print(request.args.get('updated'))
+    print(request.args.get("updated"))
 
 
 @app.route("/contact")
@@ -40,9 +51,33 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/contact/complete", methods=['GET', 'POST'])
+@app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
-    if request.method == 'POST':
-        return redirect(url_for('contact_complete'))
-    else:
-        return render_template("contact_complete.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        is_valid = True
+
+        if not username:
+            flash("ユーザー名は必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力して下さい")
+            is_valid = False
+
+        if not description:
+            flash("問合せ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+        flash("問合せありがとうございました。")
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
